585a43c6c1281a8ae9b3d70224282ab6540b3636
Nedim Basic <basicn86@gmail.com>
2024-05-11 19:18:04 -0400

    initial commit

--- CppFloatVsDouble/CppFloatVsDouble.cpp
+++ CppFloatVsDouble/CppFloatVsDouble.cpp
@@ -1,20 +1,40 @@
-// CppFloatVsDouble.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cmath>
+#include <chrono>
+
+const int ITERATIONS = 1000000000;
+
+//euclidean distance formula in 32 bit float
+float euclideanDistanceFloat(float x1, float y1, float x2, float y2)
+{
+	return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+
+//euclidean distance formula in 64 bit double
+double euclideanDistanceDouble(double x1, double y1, double x2, double y2)
+{
+	return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //run the float euclidean distance function 1 billion times and measure the time
+	auto start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < ITERATIONS; i++)
+	{
+		euclideanDistanceFloat(1.0f, 2.0f, 3.0f, 4.0f);
+	}
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> elapsed_seconds = end - start;
+	std::cout << "Float time: " << elapsed_seconds.count() << "s\n";
+
+	//run the double euclidean distance function 1 billion times and measure the time
+	start = std::chrono::high_resolution_clock::now();
+	for (int i = 0; i < ITERATIONS; i++)
+	{
+		euclideanDistanceDouble(1.0, 2.0, 3.0, 4.0);
+	}
+	end = std::chrono::high_resolution_clock::now();
+	elapsed_seconds = end - start;
+	std::cout << "Double time: " << elapsed_seconds.count() << "s\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
